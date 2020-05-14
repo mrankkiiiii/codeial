eafa3ab0459a58ddee88c4167ec59a7a09e3408d
Ankur Agarwal <agarg2311@gmail.com>
2020-05-14 22:55:29 +0530

    View is added for home

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+// set up the view engine after installing the ejs
+app.set('view engine', 'ejs');
+app.set('views','./views')
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
 
+router.use('/posts', require('./posts'));
+
 module.exports = router;
