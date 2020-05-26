49c3be22a591702e4cf751d14192c6cc178e63c8
Ankur Agarwal <agarg2311@gmail.com>
2020-05-27 01:29:44 +0530

    home page for post is restricted if the user is not signed in and the authentication is implemented in the posts route

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
-
-router.post('/create', postsController.create);
+const passport = require('passport');
+router.post('/create', passport.checkAuthentication, postsController.create);
 
 module.exports = router;
