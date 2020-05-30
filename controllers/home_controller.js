26872daa9f0395a86a3376fbf645cb7f8b2e276c
Ankur Agarwal <agarg2311@gmail.com>
2020-05-30 14:36:52 +0530

    users profile link all the friends

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25)
@@ -20,9 +21,12 @@ module.exports.home = function(req,res){
             }
         })
         .exec(function(err,posts){
-        return res.render('home',{
-            title: 'Codeial | Home',
-            posts: posts
+        User.find({}, function(err,users){
+            return res.render('home',{
+                title: 'Codeial | Home',
+                posts: posts,
+                all_users: users
+            });
         });
     });
 }
