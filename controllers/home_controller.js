bd026264e0fb519e229f68a8c6a01fa922f34809
Ankur Agarwal <agarg2311@gmail.com>
2020-05-31 01:04:29 +0530

    convert the home controller to aync await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25)
 
@@ -11,22 +11,27 @@ module.exports.home = function(req,res){
     //     });
     // });
 
-    //populate the user of each of post
-        Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function(err,posts){
-        User.find({}, function(err,users){
-            return res.render('home',{
-                title: 'Codeial | Home',
-                posts: posts,
-                all_users: users
+    try {
+         //populate the user of each of post
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
             });
+            
+        let users = await User.find({});
+
+        return res.render('home',{
+            title: 'Codeial | Home',
+            posts: posts,
+            all_users: users
         });
-    });
+
+    } catch (err) {
+     console.log('Error',err);
+     return;
+    }
 }
