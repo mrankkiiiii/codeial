b8eae49a0992fb063c41cc91d7207f1f00a9e851
Ankur Agarwal <agarg2311@gmail.com>
2020-05-30 12:53:47 +0530

    Deleting a comment from post is implemented

--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentsController = require('../controllers/comments_controller');
 const passport = require('passport');
 router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
