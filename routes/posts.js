e798616eadbc6f7cdc5c17918bb5bbac820f3d73
Ankur Agarwal <agarg2311@gmail.com>
2020-05-30 12:22:33 +0530

    Deleting a post from DB with respective there any no. of comments associate with it

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 const passport = require('passport');
 router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;
