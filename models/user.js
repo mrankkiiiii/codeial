7f67bb82d309b27ee1ff4301947b5ee836a0a9c9
Ankur Agarwal <agarg2311@gmail.com>
2020-06-07 12:17:01 +0530

    multer is installed and configured in the user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,10 +16,24 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar:{
+        type: String
     }
 } , {
     timeStamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+ 
 
 const User = mongoose.model('User', userSchema);
 
