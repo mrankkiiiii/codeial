ca66fb5d06980b700803522e861133a1fc980bdc
Ankur Agarwal <agarg2311@gmail.com>
2020-06-15 14:19:55 +0530

    token is created for the user using jwtwebtoekn library

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,23 @@
+const USer = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+module.exports.createSession = async function(req,res){
+    try {
+        let user = await USer.findOne({email: req.body.email});   
+        if(!user || user.password!=req.body.password){
+            return res.json(422, {
+                message: "Invalid username or password"
+            });
+        }
+        return res.json(200, {
+            message: 'Sign in successful, here is your token, please keep it safe',
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '100000'})
+            }
+        });
+    } catch (error) {
+        console.log('********',err);
+        return res.json(501, {
+            message: "Internal Servar Error"
+        });
+    }
+}
