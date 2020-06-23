0b4701d6136b21ba891c6640215907b9e14f5330
Ankur Agarwal <agarg2311@gmail.com>
2020-06-23 16:21:44 +0530

    post dom and home post file have some chnages for create comment using ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -15,6 +15,8 @@
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button',newPost));
+                    //call the create comment class
+                    new PostComments(data.data.post._id);
                     new Noty({
                         theme: 'sunset',
                         text: 'Post published!',
@@ -43,7 +45,7 @@
          </small>
         </p>
         <div class="post-comments">
-                 <form action="/comments/create" method="POST" id="new-comment-form-${ post._id }">
+                 <form action="/comments/create" method="POST" id="#post-${post._id}-comments-form">
                      <input type="text" name="content" placeholder="Type Here to add comment..." required>
                      <input type="hidden" name="post" value="${post._id}">
                      <input type="submit" value="Add Comment">
@@ -81,5 +83,19 @@
         });
     }
 
+    // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+    let convertPostsToAjax = function(){
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
+
+            // get the post's id by splitting the id attribute
+            let postId = self.prop('id').split("-")[1]
+            new PostComments(postId);
+        });
+    }
+
     createPost();
+    convertPostsToAjax();
 }
