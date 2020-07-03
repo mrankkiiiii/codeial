6d6e3425bdf10543c15a82e0603c00ee57ae6034
Ankur Agarwal <agarg2311@gmail.com>
2020-07-03 22:05:11 +0530

    Frienship Schema is implemented and user schema is modified by adding friendship array into it

--- models/user.js
+++ models/user.js
@@ -20,6 +20,12 @@ const userSchema = new mongoose.Schema({
     avatar:{
         type: String
     },
+    friendship: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Friendship'
+        }
+    ]
 } , {
     timeStamps: true
 });
