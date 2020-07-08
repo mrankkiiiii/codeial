bb7fc9795a1aee4cd796bbee02e07f751832e5c7
Ankur Agarwal <agarg2311@gmail.com>
2020-07-08 22:38:24 +0530

    message send and receieve in the chat room

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -23,5 +23,34 @@ class ChatEngine{
                 console.log('a user joined',data);
             });
         });
+        
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+            let newMessage = $('<li>');
+            let messageType = 'other-message';
+            if(data.user_email==self.userEmail){
+                messageType = 'self-message';
+            } 
+            newMessage.append($('<span>',{
+                'html': data.message
+            }));
+            newMessage.append($('<sub>',{
+                'html': data.user_email
+            }));
+            newMessage.addClass(messageType);
+            $('#chat-messages-list').append(newMessage);
+        });
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,5 +16,8 @@ module.exports.chatSockets = function(socketServer){
             io.in(data.chatroom).emit('user_joined',data);
         });
 
+        socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('receive_message',data);
+        });
     });
 }
