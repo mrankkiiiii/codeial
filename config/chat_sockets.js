7c3f7e23e32848989f03df29b2289191c238d13e
Ankur Agarwal <agarg2311@gmail.com>
2020-07-08 14:57:10 +0530

    begininning of new connection is established on chat engine

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,12 @@
-module.exports.chatSockets = function(){
-    
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer);    
+
+    io.on('connection', function (socket) {
+       console.log('new connection received',socket.id); 
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected');
+        });
+
+    });
 }
