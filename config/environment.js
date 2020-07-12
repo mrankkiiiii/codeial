f5de9e91626d94c43c0a836a24bc150f8c5a7ce5
Ankur Agarwal <agarg2311@gmail.com>
2020-07-12 23:06:05 +0530

    production environment variables are set

--- config/environment.js
+++ config/environment.js
@@ -17,11 +17,26 @@ const development = {
     google_client_secret: "41_0aKZ4R3hQwvvNpDrZx4QW",
     google_callback_url: "http://localhost:8000/users/auth/google/callback",
     jwt_secret: 'codeial',
-
 }
 
 const production = {
-    name:'production'
+    name:'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass:process.env.CDEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
 }
-
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
