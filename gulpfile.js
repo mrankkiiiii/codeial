4139ffa371ce12e596c6f170bc871427cc36c28a
Ankur Agarwal <agarg2311@gmail.com>
2020-07-15 16:17:27 +0530

    minimization of js done using gulp , gulp-uglify-es library

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const gulp = require('gulp');
+//css minifying
 const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
+//for renaming files
 const rev = require('gulp-rev');
+//for js minifying
+const uglify = require('gulp-uglify-es').default;
+
 
 gulp.task('css', function(done){
     console.log('minifying css');
@@ -14,6 +19,7 @@ gulp.task('css', function(done){
     gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
+     //manifest use to store the map of original file to renaming files
     .pipe(rev.manifest({
         cwd: 'public',
         merge: true
@@ -22,17 +28,14 @@ gulp.task('css', function(done){
     done();
 });
 
-gulp.task('css', function(done){
-    console.log('minifying css');
+gulp.task('js', function(done){
+    console.log('minifying js');
     //double start any folder and single start for any file
-    gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets.css'));
-    
-    gulp.src('./assets/**/*.css')
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
+   
     .pipe(rev.manifest({
         cwd: 'public',
         merge: true
